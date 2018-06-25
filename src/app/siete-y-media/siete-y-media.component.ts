b7b484a65a56529ec864ee7eb79aa9e2d696f5cb
Arnaud <atartuli@everis.com>
2018-06-25 14:41:17 +0200

    logica ok

--- src/app/siete-y-media/siete-y-media.component.ts
+++ src/app/siete-y-media/siete-y-media.component.ts
@@ -13,12 +13,15 @@ import { Cartas } from '../_modelos/cartas';
 export class SieteYMediaComponent implements OnInit {
 
   mazo: Cartas[];
+  mazoInit: Cartas[];
+  mazoInit2: Cartas[];
   cartasTiradasJugador: Cartas[] = [];
   cartasTiradasBanca: Cartas[] = [];
   carta: Cartas;
   puntosJugador: number = 0;
   puntosBanca: number = 0;
   inter: any;
+  plantarse: boolean = false;
 
 
   constructor(private mazoService: MazoService) { }
@@ -29,40 +32,78 @@ export class SieteYMediaComponent implements OnInit {
   }
 
   pedir() {
+    this.plantarse = true;
     this.carta = this.mazo.pop();
     this.cartasTiradasJugador.push(this.carta);
     this.puntosJugador += this.carta.valor;
-    // console.log(this.puntosJugador);
-  }
 
-  stop() {
-    clearInterval(this.inter);
+    if (this.puntosJugador > 7.5) {
+      this.perder();
+    }
+
+    if (this.puntosJugador == 7.5) {
+      this.juegaBanca();
+    }
   }
 
   start() {
     this.carta = this.mazo.pop();
     this.cartasTiradasBanca.push(this.carta);
     this.puntosBanca += this.carta.valor;
-    if(this.puntosBanca > this.puntosJugador){
+    if (this.puntosBanca > this.puntosJugador && this.puntosBanca <= 7.5) {
       this.stop();
+      this.perder();
+    }
+    if (this.puntosBanca > this.puntosJugador && this.puntosBanca > 7.5) {
+      this.stop();
+      this.ganar();
     }
   }
 
   juegaBanca() {
     this.inter = setInterval(() => {
       this.start();
-    }, 2000);
-    // if(this.puntosBanca > 7.5){
-    //     this.stop();
-    //  }
+    }, 1500);
+  }
+
+  perder() {
+    setTimeout(() => {
+
+      alert('has perdido');
+      this.puntosJugador = 0;
+      this.puntosBanca = 0;
+      this.mazoInit = this.cartasTiradasBanca.concat(this.cartasTiradasJugador);
+      this.mazoInit2 = this.mazo.concat(this.mazoInit);
+      this.mazo = this.mazoInit2;
+      this.mazoService.shuffle(this.mazo);
+      this.cartasTiradasJugador = [];
+      this.cartasTiradasBanca = [];
+      this.plantarse = false;
+    }, 500);
 
   }
 
-  stopp() {
-    this.stop();
+  ganar() {
+    setTimeout(() => {
+
+      alert('has ganado');
+      this.puntosJugador = 0;
+      this.puntosBanca = 0;
+      this.mazoInit = this.cartasTiradasBanca.concat(this.cartasTiradasJugador);
+      this.mazoInit2 = this.mazo.concat(this.mazoInit);
+      this.mazo = this.mazoInit2;
+      this.mazoService.shuffle(this.mazo);
+      this.cartasTiradasJugador = [];
+      this.cartasTiradasBanca = [];
+      this.plantarse = false;
+    }, 500);
   }
 
-  
+  stop() {
+    clearInterval(this.inter);
+  }
+
+
 
 
 
